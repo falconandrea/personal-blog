62c67003445e8994b48dbb6cd6e4c4e8fb44b884
Andrea Falcon <falcon.andrea88@gmail.com>
2021-10-27 19:08:53 +0200

    feat: update posts api with search and tag

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -6,14 +6,24 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\PostResource;
 use Illuminate\Http\Request;
 use App\Models\Post;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class PostController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $posts = Post::where('published', '=', 1)->get();
-        return PostResource::collection($posts);
+        $posts = Post::where('published', '=', 1);
+        if ($request->has('tag') && $request->input('tag') != '') {
+            $posts = $posts->whereHas('tags', function (Builder $query) use ($request) {
+                $query->where('slug', $request->input('tag'));
+            });
+        }
+        if ($request->has('search') && $request->input('search') != '') {
+            $posts = $posts->where('title', 'like', '%' . $request->input('search') . '%')->orWhere('text', 'like', '%' . $request->input('search') . '%');
+        }
+
+        return PostResource::collection($posts->get());
     }
 
     public function show($post)
