d536918e392fdf01de216cb827db9688eecc3985
Andrea Falcon <83451204+falconandrea@users.noreply.github.com>
2021-12-03 18:35:48 +0100

    feat: install inertia and update related files

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix');
+const mix = require('laravel-mix')
 
 /*
  |--------------------------------------------------------------------------
@@ -12,6 +12,8 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+  .vue(3)
+  .postCss('resources/css/app.css', 'public/css', [
+    //
+  ])
+  .version()
