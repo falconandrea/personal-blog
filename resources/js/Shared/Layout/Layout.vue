154979c477faf8c683c6228ca6b0675a84274de2
Andrea Falcon <83451204+falconandrea@users.noreply.github.com>
2021-12-06 18:19:51 +0100

    feat: add Head for SEO

--- resources/js/Shared/Layout/Layout.vue
+++ resources/js/Shared/Layout/Layout.vue
@@ -1,4 +1,8 @@
 <template>
+    <Head>
+        <title>Blog</title>
+        <meta name="description" content="Mi chiamo Andrea Falcon e sono uno sviluppatore Web Full-Stack. Ho deciso di creare questo blog per tener traccia delle tecnologie e conoscenze che apprendo durante la mia formazione." head-key="description" />
+    </Head>
     <Header />
     <main class="container flex flex-col lg:flex-row mx-auto pb-4">
        <slot />
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,5 @@
 import { createApp, h } from 'vue'
-import { createInertiaApp, Link } from '@inertiajs/inertia-vue3'
+import { createInertiaApp, Link, Head } from '@inertiajs/inertia-vue3'
 import { InertiaProgress } from '@inertiajs/progress'
 import Layout from './Shared/Layout/Layout.vue'
 
@@ -13,8 +13,10 @@ createInertiaApp({
     createApp({ render: () => h(App, props) })
       .use(plugin)
       .component('Link', Link)
+      .component('Head', Head)
       .mount(el)
-  }
+  },
+  title: title => `${title} - AndreaFalcon.Dev`
 })
 
 InertiaProgress.init({
