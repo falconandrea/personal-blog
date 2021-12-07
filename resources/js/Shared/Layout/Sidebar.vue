2e1eee74bf6998e4716d1a751546ab7860f0abe0
Andrea Falcon <83451204+falconandrea@users.noreply.github.com>
2021-12-07 15:00:34 +0100

    feat: move search form in component

--- resources/js/Shared/Layout/Sidebar.vue
+++ resources/js/Shared/Layout/Sidebar.vue
@@ -2,14 +2,7 @@
   <div class="p-4 w-full lg:w-1/3">
     <h3 class="mt-4 mb-2 text-xl">About</h3>
     <p class="pt-2 text-gray-500">Ciao, mi chiamo Andrea Falcon e sono uno sviluppatore Web Full-Stack.<br>Ho deciso di creare questo blog per tener traccia delle tecnologie e conoscenze che apprendo durante la mia formazione.<br>Cercherò di documentare e annotare tutto il possibile.</p>
-
-    <h3 class="mt-8 mb-2 text-xl">Cerca tra i post</h3>
-    <form method="GET" :action="formPath" class="flex flex-row">
-      <input id="search-input" class="border-2 rounded-xl px-4 py-2" type="text" name="search" placeholder="Laravel, Vue ..."/>
-      <button class="px-4 text-white bg-blue-300 rounded-xl ml-2 hover:bg-blue-400" type="submit">
-          Cerca
-      </button>
-    </form>
+    <SearchForm />
     <h3 class="mt-8 mb-2 text-xl">Tags</h3>
     <ListTags :tags=tags></ListTags>
   </div>
@@ -17,10 +10,11 @@
 
 <script>
 import ListTags from '../ListTags.vue'
+import SearchForm from '../SearchForm.vue'
 import { usePage } from '@inertiajs/inertia-vue3'
 export default {
   components: {
-    ListTags
+    ListTags, SearchForm
   },
   data() {
       return {
---
+++ resources/js/Shared/SearchForm.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h3 class="mt-8 mb-2 text-xl">Cerca tra i post</h3>
+        <form @submit.prevent="submitForm" class="flex flex-row">
+            <input id="search-input" class="border-2 rounded-xl px-4 py-2" type="text" v-model="formData.search" placeholder="Laravel, Vue ..."/>
+            <button class="px-4 text-white bg-blue-300 rounded-xl ml-2 hover:bg-blue-400" type="submit">
+                Cerca
+            </button>
+        </form>
+    </div>
+</template>
+
+<script setup>
+import { useForm } from '@inertiajs/inertia-vue3'
+
+let formData = useForm({
+    search: ''
+})
+let submitForm = () => {
+    formData.get('/', formData);
+}
+</script>
