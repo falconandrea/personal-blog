11fdc710b865e680fa9ea235b20713f45d512209
Andrea Falcon <83451204+falconandrea@users.noreply.github.com>
2021-12-14 17:45:24 +0100

    fix: description seo duplicated

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="it">
   <head>
-    {!! str_replace('<title>','<title inertia>', Meta::toHtml()) !!}
+    {!! str_replace(['<title>', '<meta name="description"'], ['<title inertia>', '<meta inertia name="description"'], Meta::toHtml()) !!}
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
     <script src="{{ mix('/js/app.js') }}" defer></script>
   </head>
