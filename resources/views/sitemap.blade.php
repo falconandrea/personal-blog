506b14a2974f4f8416faafe1bca1381f8af963b8
Andrea Falcon <83451204+falconandrea@users.noreply.github.com>
2022-01-12 16:35:48 +0100

    fix: sitemap

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,7 +1,7 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
-        <loc>https://localhost/</loc>
+        <loc>{{ url('/') }}</loc>
         <lastmod>{{ $indexTimestamp }}</lastmod>
         <changefreq>weekly</changefreq>
         <priority>0.8</priority>
