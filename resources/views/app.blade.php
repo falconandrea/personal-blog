9a4a17fe22f68caf4d060c4d58f5d46a4f277160
Andrea Falcon <83451204+falconandrea@users.noreply.github.com>
2022-01-17 18:18:56 +0100

    feat: new google tag manager integration

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,8 +4,10 @@
     {!! str_replace(['<title>', '<meta name="description"'], ['<title inertia>', '<meta inertia name="description"'], Meta::toHtml()) !!}
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
     <script src="{{ mix('/js/app.js') }}" defer></script>
+    @include('googletagmanager::head')
   </head>
   <body>
+    @include('googletagmanager::body')
     @inertia
   </body>
 </html>
