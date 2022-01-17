7d1d3020a1fbdcf066edcc488ae1a5069a08f36b
Andrea Falcon <83451204+falconandrea@users.noreply.github.com>
2022-01-17 17:56:42 +0100

    feat: remove vue-gtag

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,11 @@
 import { createApp, h } from 'vue'
-import { createInertiaApp, Link, Head, usePage } from '@inertiajs/inertia-vue3'
+import { createInertiaApp, Link, Head } from '@inertiajs/inertia-vue3'
 import { InertiaProgress } from '@inertiajs/progress'
 import { library, dom } from '@fortawesome/fontawesome-svg-core'
 import { faLinkedinIn, faGithub } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import VueCodeHighlight from 'vue-code-highlight'
-import VueGtag from 'vue-gtag'
 import Layout from './Shared/Layout/Layout.vue'
-import { Inertia } from '@inertiajs/inertia'
 
 library.add(faLinkedinIn)
 library.add(faGithub)
@@ -23,15 +21,6 @@ createInertiaApp({
     createApp({ render: () => h(App, props) })
       .use(plugin)
       .use(VueCodeHighlight)
-      .use(VueGtag, {
-        config: {
-          id: props.initialPage.props.gtag,
-          params: {
-            send_page_view: false
-          }
-        },
-        pageTrackerScreenviewEnabled: true
-      })
       .component('Link', Link)
       .component('Head', Head)
       .component('font-awesome-icon', FontAwesomeIcon)
