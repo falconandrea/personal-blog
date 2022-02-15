0e660e21e595af468e47b1e8e85945a352de33a5
Andrea Falcon <83451204+falconandrea@users.noreply.github.com>
2022-02-15 18:52:42 +0100

    rollback: remove schedule backup tasks

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,8 +24,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('backup:clean')->daily()->at('20:00');
-        $schedule->command('backup:run')->daily()->at('20:30');
+        // $schedule->command('backup:clean')->daily()->at('20:00');
+        // $schedule->command('backup:run')->daily()->at('20:30');
     }
 
     /**
