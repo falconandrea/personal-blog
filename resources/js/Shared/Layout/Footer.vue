7c212bef675b81b8f2e06a4cf9485c8f166fc87c
Andrea Falcon <83451204+falconandrea@users.noreply.github.com>
2022-10-10 19:15:59 +0200

    feat: comment import iubenda files

--- resources/js/Shared/Layout/Footer.vue
+++ resources/js/Shared/Layout/Footer.vue
@@ -3,7 +3,7 @@
     <div class="container flex mx-auto p-4">
       <p class="w-8/12">
         <small>&copy; 2021 Falcon Andrea / Made with Laravel, InertiaJs and Tailwind.</small>
-        <Iubenda v-if="showIubenda"/>
+        <!-- <Iubenda/>-->
       </p>
       <p class="w-4/12 text-right">
         <a class="mr-4" href="https://www.linkedin.com/in/andrea-falcon-fullstack-developer/" title="Linkedin" target="_blank">
@@ -18,16 +18,12 @@
 </template>
 
 <script>
+/*
 import Iubenda from '@/Shared/Iubenda.vue'
-import { usePage } from '@inertiajs/inertia-vue3'
 export default {
     components: {
         Iubenda
-    },
-    data() {
-      return {
-          showIubenda: usePage().props.value.iubenda && usePage().props.value.iubenda != ''
-      }
     }
 }
+*/
 </script>
